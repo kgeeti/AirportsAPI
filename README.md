42fd79f2a649fe86f84ef3a2c746ef184a4b4b4f
KGe <kge@terra.com.br>
2025-02-06 16:27:42 -0300

    # Término da Fase 1 - Projeto inicial start.spring.io; - Configuração MainClass; - Configuração application.properties; - Configuração application-test.properties; 	- Configuração Database H2 	- Configuração de exibição de console; - Inicialização da classe Airport

---
+++ README.md
@@ -0,0 +1,35 @@
+# AirportsAPI
+
+Tutorial para criação de API utilizando Java + Spring
+
+Este projeto contempla:
+
+- Ambiente de teste e ambiente de desenvolvimento
+- Database H2 para teste
+- Persistência de dados com Spring JPA
+
+
+O projeto é auto guiado e cada fase está commitada. Portanto, para voltar a qualquer uma das fase utilize
+
+```
+git checkout hashDoCommit
+```
+
+## Para habilitar o output de console colorido:
+
+Menu Tools → Options → Java → Maven
+
+No campo “Global Execution Policy” adicionar
+
+```
+-Dstyle.color=always
+```
+
+As opções são:
+
+- auto
+- never
+- always
+
+Adicionar ao final dos parâmetros, respeitando a separação por espaço.
+
---
+++ src/main/java/br/eti/kge/airports/entities/Airport.java
@@ -0,0 +1,45 @@
+package br.eti.kge.airports.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Classe para representar um Airport
+ *
+ * @author KGe
+ */
+@Entity
+@Table(name = "airport")
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String city;
+    private String country;
+    
+    @Column(name = "iatacode")
+    private String iataCode;
+    
+    @Column(name = "icaocode")
+    private String icaoCode;
+    
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    
+    @Column(name = "offsetutc")
+    private double offsetFromUTC;
+    
+    @Column(name = "dstcode")
+    private String dstCode;
+    private String timezone;
+    
+    
+    
+}
